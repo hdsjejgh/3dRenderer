f67c7d255ee4adbabc0a3852af28a336bb9623eb
hdsjejgh <aayanahmad08@hotmail.com>
2025-03-22 11:43:10 -0400

    Organized files, added some more models, added 2 new shaders

--- main.py
+++ main.py
@@ -3,31 +3,31 @@ from shapes import *
 import pygame
 import random
 from parameters import *
-
+from shaders import *
 
 c=Cube(
         coords=[[-2,2,2],[2,2,2],[2,-2,2],[-2,-2,2],[-2,2,6],[2,2,6],[2,-2,6],[-2,-2,6],],
          faces=[[0,1,2,3],[4,5,6,7],[0,4,7,3],[1,5,6,2],[0,1,5,4],[2,3,7,6],]
     )
-c=OBJFile("Shambler.obj")
-c.scaleCoords(-2.75)
+c=OBJFile("models/Shambler.obj")
+c.scaleCoords(-3)
 c.update2dCoords()
 
 def TransformationLoop():
     #c.rotateCoords('x', 6)
-    c.rotateCoords('y', -6)
+    c.rotateCoords('y', -2)
     #c.rotateCoords('z', math.e)
     c.update2dCoords()
-    sleep(0.0)
+    #sleep(0.0)
 
-def display(shape):
+def display(shape,shader):
     def center(x):
         x=list(x)
         x[0]+=WIDTH/2
         x[1]+=HEIGHT/2
         return x
     for face in shape.faces:
-        pygame.draw.polygon(screen, tuple(random.randint(0, 255) for i in range(3)), list(map(center,face.TwoDCoords)))
+        pygame.draw.polygon(screen, shader(face), list(map(center,face.TwoDCoords)))
     pygame.display.flip()
 
 if __name__ == '__main__':
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     while running:
 
         for event in pygame.event.get():
-            if event == pygame.QUIT:
+            if event.type == pygame.QUIT:
                 running=False
 
 
@@ -51,7 +51,7 @@ if __name__ == '__main__':
         c.update2dCoords()
 
 
-        display(c)
+        display(c,distShader(0.03))
         clock.tick(FPS)
 
 
---
+++ shaders.py
@@ -0,0 +1,22 @@
+import random
+import math
+
+#(for the random shader)
+COLORS = tuple(tuple(random.randint(0,255) for ii in range(3)) for i in range(100))
+
+#Fragment Shaders
+
+def randomShader(): #assigns random color to each face (based on id), face stays that random color
+    def wrapper(face):
+        return COLORS[face.id%len(COLORS)]
+    return wrapper
+
+#Shader that mades faces darker furter away, and lighter closer
+def distShader(mult=0.05,base=(255,255,255)): #mult controls steepness of sigmoid function
+    #steeper mult = steeper sigmoid = quicker dropoff of brightness
+    def wrapper(face):
+        val = 255 * (1-  (1/(1+math.exp(-mult*face.z)))   )
+        val = min(255,val)
+        #print(val)
+        return tuple(map(lambda x:x*(val/255),base))
+    return wrapper
