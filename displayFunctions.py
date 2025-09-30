5b4f6fd8df678dbd4551fc2273301319bcbe09d7
hdsjejgh <aayanahmad08@hotmail.com>
2025-09-30 16:49:55 -0400

    fixed bug where mouse movements broke

--- displayFunctions.py
+++ displayFunctions.py
@@ -5,51 +5,6 @@ import numpy as np
 from shaders import *
 from lightFunctions import *
 
-
-#Handles all mouse functions
-#Dragging rotates everything in the expected manner
-#Scrolling up zooms in
-#Scrolling down zooms out
-def mouse_callback(event, x, y, flags, params):
-    global Model
-    global prevx,prevy
-
-    #If mouse pressed
-    if event == cv.EVENT_LBUTTONDOWN:
-        prevx,prevy = x,y
-    #If mouse unpressed, erase previous position data
-    elif event == cv.EVENT_LBUTTONUP:
-        prevx,prevy = -1,-1
-    #If mouse moved and previous position data exists, rotate everything
-    elif event == cv.EVENT_MOUSEMOVE:
-        if prevx!=-1 and prevy !=-1:
-            dx = x-prevx
-            dy = y-prevy
-            prevx = x
-            prevy = y
-
-            #Rotations are scaled such that dragging across half the screen rotats the model 90 degrees
-
-            lightRot('y',(-dx/parameters.WIDTH)*180)
-            Model.rotate('y',(-dx/parameters.WIDTH)*180,selfcc=False)
-
-            lightRot('x', (dy / parameters.HEIGHT) * 180)
-            Model.rotate('x', (dy / parameters.HEIGHT) * 180,selfcc=False)
-
-            Model.updateFaces()
-
-    #If mouse scrolled
-    elif event == cv.EVENT_MOUSEWHEEL:
-        #If scrolled up
-        if flags>0:
-            Model.scale(1.1,[0,0,0])
-            lightScale(1.1)
-        #If scrolled down
-        elif flags<0:
-            Model.scale(1/1.1,[0,0,0])
-            lightScale(1/1.1)
-
-
 #Shader rasterization functions
 
 
--- main.py
+++ main.py
@@ -29,11 +29,62 @@ timestep = 0
 #Number of valid faces in current frame (for diagnostic purposes)
 num_faces = 0
 
+#Handles all mouse functions
+#Dragging rotates everything in the expected manner
+#Scrolling up zooms in
+#Scrolling down zooms out
+def mouse_callback(event, x, y, flags, params):
+    global Model
+    global prevx,prevy
+
+    #If mouse pressed
+    if event == cv.EVENT_LBUTTONDOWN:
+        prevx,prevy = x,y
+    #If mouse unpressed, erase previous position data
+    elif event == cv.EVENT_LBUTTONUP:
+        prevx,prevy = -1,-1
+    #If mouse moved and previous position data exists, rotate everything
+    elif event == cv.EVENT_MOUSEMOVE:
+        if prevx!=-1 and prevy !=-1:
+            dx = x-prevx
+            dy = y-prevy
+            prevx = x
+            prevy = y
+
+            #Rotations are scaled such that dragging across half the screen rotats the model 90 degrees
+
+            lightRot('y',(-dx/parameters.WIDTH)*180)
+            Model.rotate('y',(-dx/parameters.WIDTH)*180,selfcc=False)
+
+            lightRot('x', (dy / parameters.HEIGHT) * 180)
+            Model.rotate('x', (dy / parameters.HEIGHT) * 180,selfcc=False)
+
+            Model.updateFaces()
+
+    #If mouse scrolled
+    elif event == cv.EVENT_MOUSEWHEEL:
+        #If scrolled up
+        if flags>0:
+            Model.scale(1.1,[0,0,0])
+            lightScale(1.1)
+        #If scrolled down
+        elif flags<0:
+            Model.scale(1/1.1,[0,0,0])
+            lightScale(1/1.1)
+
 #Sets up mouse callback function
 cv.setMouseCallback("3d Render",mouse_callback)
 
+
+#CHANGE THESE THINGS ##########################################
+
+
 #The model loaded
 Model = OBJ_File("models/Shambler.obj",reverseNormals=True)
+
+# Which shader rasterizing function to use
+# All are in the displayFunctions file
+SHADER = phong
 
 #Transformations to be done to the model before anything
 def pretransformation():
@@ -52,11 +103,11 @@ def TransformationLoop():
     # Model.twist('z', 1, 0.01, center=100)
     lightRot('y',1)
 
-if __name__ == '__main__':
 
-    #Which shader rasterizing function to use
-    #All are in the displayFunctions file
-    SHADER = phong
+#######################################################
+
+
+if __name__ == '__main__':
 
     #T is used to time different things for debugging
     t = time()
